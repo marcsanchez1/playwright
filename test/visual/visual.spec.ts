15a6307b4aad615b140bea7794ccaebc7d8f3423
Marc <marcsanchez@me.com>
2023-06-20 22:24:03 -0400

    added visual tests

--- test/visual/visual.spec.ts
+++ test/visual/visual.spec.ts
@@ -5,5 +5,13 @@ test.describe('Visual regression testing example', () => {
     await page.goto('https://www.example.com')
     
     expect(await page.screenshot()).toMatchSnapshot('homepage.png')
+  })
+
+  test('Single element snapshot', async ({page}) => {
+    await page.goto('https://www.example.com')
+
+    const pageElement = await page.$('h1')
+    expect(await pageElement.screenshot()).toMatchSnapshot('page-title.png')
+    
   });
 });
