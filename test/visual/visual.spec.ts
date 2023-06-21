540f240b9182355ae422233df0430490d74589a7
Marc <marcsanchez@me.com>
2023-06-20 22:19:56 -0400

    added visual screenshot tests

---
+++ test/visual/visual.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('Visual regression testing example', () => {
+  test('Full page snapshot', async ( {page} ) => {
+    await page.goto('https://www.example.com')
+    
+    expect(await page.screenshot()).toMatchSnapshot('homepage.png')
+  });
+});
